809f6c72a16797bcd8f35a41c6a7e083edba961f
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2024-08-10 20:57:33 +0300

    Consumer

---
+++ RabbitMq.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Baglanti olusturma
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wqbvawvt:aTi_gA9eIEH4Ot3LV4PDfWHGFZieaNK-@shark.rmq.cloudamqp.com/wqbvawvt");
+
+//Baglanti Aktiflestirme
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Olusturma
+channel.QueueDeclare(queue: "example-queeue", exclusive:false); //Consumer"da da kuyruk puvlisherdaki ile birebir ayni yapilandirmada yapilmalidir.
+
+//Queue den mesaj okuma
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "example-queeue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruga gelen mesajin islendigi yerdir.
+    //e.Body : Kuyruktaki mesajin verisini butunsel olarak getirecektir.
+    //e.Body.Span veua e.Body.ToArray() : Kuyruktaki mesaji byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
