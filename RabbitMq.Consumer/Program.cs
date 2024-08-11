6789cf95dc65932ce5108acc95d170f8a2ee5b7e
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2024-08-11 17:50:17 +0300

    Added
    MessageAcknowledgment ayari yapildi ve Fair Dispatch eklendi

--- RabbitMq.Consumer/Program.cs
+++ RabbitMq.Consumer/Program.cs
@@ -13,16 +13,18 @@ using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
 //Queue Olusturma
-channel.QueueDeclare(queue: "example-queeue", exclusive:false); //Consumer"da da kuyruk puvlisherdaki ile birebir ayni yapilandirmada yapilmalidir.
+channel.QueueDeclare(queue: "example-queeue", exclusive:false,durable:true); //Consumer"da da kuyruk puvlisherdaki ile birebir ayni yapilandirmada yapilmalidir.
 
 //Queue den mesaj okuma
 EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
-channel.BasicConsume(queue: "example-queeue", false, consumer);
+channel.BasicConsume(queue: "example-queeue", autoAck:false, consumer);
+channel.BasicQos(0, 1, false); //Fair Dispatch icin verilerin daha duzgun gitmesini saglar
 consumer.Received += (sender, e) =>
 {
     //Kuyruga gelen mesajin islendigi yerdir.
     //e.Body : Kuyruktaki mesajin verisini butunsel olarak getirecektir.
     //e.Body.Span veua e.Body.ToArray() : Kuyruktaki mesaji byte verisini getirecektir.
+    channel.BasicAck(e.DeliveryTag,false);
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
 };
 
